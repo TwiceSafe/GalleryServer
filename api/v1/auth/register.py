651f023dcdd3b091805c2eb975ec65bef7e68bfa
Vyacheslav Bytsko <169024760+vyacheslavbytsko@users.noreply.github.com>
2024-12-05 00:38:21 +0300

    fix

--- api/v1/auth/register.py
+++ api/v1/auth/register.py
@@ -5,8 +5,9 @@ from classes.user import User, UserStatus, user_db_session_maker
 
 async def search(username: str, password: str):
     async with user_db_session_maker() as session:
-        user = User(user_id=str(uuid.uuid4()))
+        user = User()
 
+        user.user_id = str(uuid.uuid4())
         user.status = UserStatus.ACTIVE
         user.username = username
         user.password = password

c7e1a9699688b5e6ca77af3911a908dc7abf549b
Vyacheslav Bytsko <169024760+vyacheslavbytsko@users.noreply.github.com>
2024-12-04 17:16:14 +0300

    minor fix

--- api/v1/repository/head.py
+++ api/v1/repository/head.py
@@ -1,7 +1,7 @@
 from classes.user import get_user_from_token_info, user_db_session_maker
 
 
-async def search(token_info, repository_name):
+async def search(token_info: dict, repository_name: str):
     async with user_db_session_maker() as session:
         user = await get_user_from_token_info(session, token_info)
         head = user.get_head(repository_name)

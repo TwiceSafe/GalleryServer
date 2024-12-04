3079f7e8bfdf882501324a9762a480782fcd0cc7
Vyacheslav Bytsko <169024760+vyacheslavbytsko@users.noreply.github.com>
2024-12-04 19:00:40 +0300

    continuation of fix

--- api/v1/repository/commit.py
+++ api/v1/repository/commit.py
@@ -1,5 +1,10 @@
-def get(repository_name: str, commit_id: str):
+from classes.user import get_user_from_token_info, user_db_session_maker
+
+
+async def get(token_info: dict, repository_name: str, commit_id: str):
     return "держи"
 
-def post(repository_name: str, commit: dict):
-    return "добавили"
+async def post(token_info: dict, repository_name: str, commit: dict):
+    async with user_db_session_maker() as session:
+        user = await get_user_from_token_info(session, token_info)
+        return "добавили"

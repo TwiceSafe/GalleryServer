44e9af1939b6f6a1210602fea228822cabbcb6ae
Vyacheslav Bytsko <169024760+vyacheslavbytsko@users.noreply.github.com>
2024-12-07 23:55:16 +0300

    now /hello can send error

--- api/hello.py
+++ api/hello.py
@@ -1,5 +1,20 @@
 from typing import Optional
 
 
-def search(v: Optional[int] = None):
-    return {"status": 200, "hello": "Hi!", "api_version": 1}
+def search(error: Optional[int] = None, v: Optional[int] = None):
+    if error:
+        return {
+            "status": 400,
+            "error": {
+                "name": "badrequest",
+                "description": "You sent bad request on purpose."
+            }
+        }, 400
+
+    return {
+        "status": 200,
+        "response": {
+            "hello": "Hi!",
+            "api_version": 1
+        }
+    }
